83f1c6dd65a1e6eef964f09a8f8d0317c1f1499e
sunshuaisky <sunshuaisky123@163.com>
2017-12-29 12:09:29 +0800

    vue_axios

---
+++ src/utils/api.js
@@ -0,0 +1,6 @@
+export default {
+	leagues: '/league/getLeagues', // 获取联赛
+	Tpoint: '/league/totalrank/', //获取总积分
+	Match: '/league/match/', //获取比赛列表
+	MRanks: '/league/matchrank/' //获取比赛成绩
+}
---
+++ src/utils/http.js
@@ -0,0 +1,85 @@
+'use strict'
+
+import axios from 'axios'
+import qs from 'qs'
+
+axios.interceptors.request.use(config => {
+  // loading
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+
+axios.interceptors.response.use(response => {
+  return response
+}, error => {
+  return Promise.resolve(error.response)
+})
+
+function checkStatus(response) {
+  // loading
+  // 如果http状态码正常，则直接返回数据
+  if (response && (response.status === 200 || response.status === 304 || response.status === 400)) {
+    return response
+    // 如果不需要除了data之外的数据，可以直接 return response.data
+  }
+  // 异常状态下，把错误信息返回去
+  return {
+    status: -404,
+    msg: '网络异常'
+  }
+}
+
+function checkCode(res) {
+  // 404
+  if (res.status === -404) {
+    console.log(res.msg)
+  }
+  // 后端异常根据情况添加
+
+  return res
+}
+
+export default {
+  post(url, data) {
+    return axios({
+      method: 'post',
+      baseURL: '/api',
+      url,
+      data: qs.stringify(data),
+      timeout: 10000,
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      }
+    }).then(
+      (response) => {
+        return checkStatus(response)
+      }
+    ).then(
+      (res) => {
+        return checkCode(res)
+      }
+    )
+  },
+  get(url, params) {
+    return axios({
+      method: 'get',
+      baseURL: '/api',
+      url,
+      params, // get 请求时带的参数
+      timeout: 10000,
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest'
+      }
+    }).then(
+      (response) => {
+        return checkStatus(response)
+      }
+    ).then(
+      (res) => {
+        return checkCode(res)
+      }
+    )
+  }
+}
